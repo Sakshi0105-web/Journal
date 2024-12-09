abfcdac799ef3bef5010ed236aa7a43ff33fcc73
Sakshi0105-web <Sakshiasindgi@gmail.com>
2024-12-09 11:11:49 +0100

    Added files

---
+++ Journal/Journal/JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalApp.swift
+//  Journal
+//
+//  Created by Sakshi Anil Sindgi on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
